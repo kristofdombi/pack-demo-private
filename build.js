1648d885e47340aab04d5a6328403e911fce18b3
Peter Kota <kotapeter@gmail.com>
2022-01-21 11:21:41 +0100

    init

---
+++ build.js
@@ -0,0 +1,2 @@
+console.log(`I'm running after npm install. This is still buildtime`)
+console.log('BUILDTIME env var:', process.env.BUILDTIME)
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const server = express()
+
+server.get('/', (req, res) => {
+  console.log('RUNTIME_ENV', process.env.RUNTIME)
+  res.send('Hello Kinsta 3')
+})
+const port = process.env.PORT || 8080
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
