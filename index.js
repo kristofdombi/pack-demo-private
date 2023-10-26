d1e0570f60d893942c301b9dc98471a4dbf9ec04
Peter Kota <kotapeter@gmail.com>
2023-10-26 11:18:40 +0200

    add interval log

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ server.get('/', (req, res) => {
 })
 const port = process.env.PORT || 8080
 server.listen(port, () => {
+  setInterval(() => {
+    console.log('Pack Demo heartbeat 🧘')
+  }, 2000)
   console.log('NODE_ENV:', process.env.NODE_ENV)
   console.log(`Example app1 listening at http://localhost:${port}`)
 })
