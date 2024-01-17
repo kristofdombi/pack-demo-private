a67b7737eacfac8964551e0a06f101d04aee9215
Peter Kota <kotapeter@gmail.com>
2024-01-17 09:17:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = express()
 
 server.get('/', (req, res) => {
   console.log('RUNTIME_ENV', process.env.RUNTIME)
-  res.send('Hello Kinsta Private asd')
+  res.send('Hello Kinsta Priasdvate asd')
 })
 const port = process.env.PORT || 8080
 server.listen(port, () => {
