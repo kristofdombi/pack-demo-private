ae1e2a177a92547029961db2724fdbfb98a3d266
Peter Kota <kotapeter@gmail.com>
2024-02-29 14:47:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 8080
 server.listen(port, () => {
   setInterval(() => {
     console.log('Pack Demo heartbeat 🧘')
-  }, 5000)
+  }, 4000)
   console.log('NODE_ENV:', process.env.NODE_ENV)
   console.log(`Example app1 listening at http://localhost:${port}`)
 })
