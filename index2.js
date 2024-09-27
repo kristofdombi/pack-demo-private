f3d2b474fe5391a2c9074c36d31d2c9c2486eab9
Peter Kota <kotapeter@gmail.com>
2024-09-27 13:09:58 +0200

    Update index2.js

--- index2.js
+++ index2.js
@@ -6,6 +6,9 @@ server.get('/', (req, res) => {
   console.log('RUNTIME_ENV', process.env.RUNTIME)
   res.send('Hello Kinsta Private 2')
 })
+server.get('/t', (req, res) => {
+  console.log('timeout asd')
+})
 const port = process.env.PORT || 8080
 server.listen(port, () => {
   console.log(`Example app 2 YO listening at http://localhost:${port}`)
