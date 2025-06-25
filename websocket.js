7e6c152174fdfbbe3ff40c3ae3f5033406dbdbc8
Kristóf Dombi <dombi.kristof@gmail.com>
2025-06-25 16:34:11 +0200

    chore: update package.json

--- websocket.js
+++ websocket.js
@@ -1,5 +1,5 @@
-import http from 'http';
-import { WebSocketServer } from 'ws';
+const http = require('http');
+const { WebSocketServer } = require('ws')
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
